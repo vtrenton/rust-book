9945794aa3b666e279aa1d10b072ac6ed5ef5bc2
Trent V <trenton.vanderwert@gmail.com>
2023-12-18 23:28:30 -0600

    started ch04

---
+++ ch04/ownership/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let s = "Hello"; // string literal - string is hardcorded in the program
+
+    // String type in rust is stored on the heap
+    let s = String::from("Hello")
+    
+}
