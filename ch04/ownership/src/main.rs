e57b6d4685276226a7a88de47589c36761a3339a
Trent V <trenton.vanderwert@gmail.com>
2023-12-19 19:13:51 -0600

    moved some stuff around and new ownership program

--- ch04/ownership/src/main.rs
+++ ch04/ownership/src/main.rs
@@ -1,106 +1,24 @@
 fn main() {
-    let s = "Hello"; // string literal - string is hardcorded in the program
+    let s = String::from("Hello"); // s comes into scope
 
-    // String type in rust is stored on the heap
-    // This string can be mutated
-    let mut s = String::from("Hello");
-    s.push_str(", world!"); // push_str appends a literal to the string
+    takes_ownership(s); // s now is passed into takes_ownership
+                        // s has now changed scope - it can no longer be referenced
+    // this would fail with a borrow after move
+    //println!("{}", s);
 
-    // prints Hello World
-    println!("{}", s);
-
-    // an int is a static predictible hard-coded value
-    let x = 5; // assign x to 5
-    let y = x; // take the value of x and copy it into y
-
-    // String handles this much differently
-    let s1 = String::from("Hello");
-    let s2 = s1;
-
-    // The Stack contains a pointer to the heap
-    // A size and capacity as well
-    // on the heap contains the actual content of the String
-
-    /*
-    Stack               Heap
-    +------+           +------+
-    | s1   | ---->     | hello |
-    | len:5|           +------+
-    | cap:5|
-    +------+
-     */
-
-    // len is the total bytes used by the string
-    // cap is the total amount of memory in bytes allocated on the heap
-
-    // when we assign s1 to s2 all of the string data 
-    // on the stack is copied from s1 to s2
-    // including the pointer to the data on the stack
-
-    /*
-    Stack               Heap
-    +------+           +------+
-    | s1   | ----+     | hello |
-    | len:5|     |     +------+
-    | cap:5|     |
-    +------+     |
-    | s2   | ----+
-    | len:5|
-    | cap:5|
-    +------+
-     */
-
-    // if both s1 and s2 leave scope
-    // since both are trying to free the same address
-    // this leads to a "Double-Free error"
-
-    // Rust will treat s1 as invalid after s2 is declared
-    // So when s1 leaves scope - nothing will be done
-
-    // This will break
-    //println!("{}, World!", s1);
-
-    // s1 is MOVED to s2
-    // meaning only s2 is accessible
-    // this is called a 'shallow copy'
-    /*
-    Stack               Heap
-    +------+           +------+
-    | s1   |           | hello |
-    | len:5|           +------+
-    | cap:5|           
-    |(invalid)         
-    +------+           
-    | s2   | ---->     
-    | len:5|           
-    | cap:5|           
-    +------+
-    */
-
-    // Rust will never perform "deep" copies automatically
-    // this helps with runtitme performance as well as safety
-
-    // If I do in fact want a 'deep copy'
-    // I can use 'clone'
-    let str1 = String::from('Hello');
-    let str2 = str1.clone();    
-
-    println!("str1 = {}, str2 = {}", str1, str2);
-
-    // in memory the value on the heap is being reallocated as well:
-    /*
-    Stack               Heap
-    +------+           +------+
-    | str1 | ---->     | hello |
-    | len:5|           +------+
-    | cap:5|           
-    +------+           +------+
-    | str2 | ---->     | hello |
-    | len:5|           +------+
-    | cap:5|
-    +------+
-    */
-
+    let x = 5; // an i32 comes into scope
+               // i32 implements the copy
     
+    makes_copy(x); // pass x into the function
+    
+    // totally valid
+    println!("after copy the value is still here local to main: {}", x);
+} // here x goes out of scope, s has already been moved into the take_ownership function
 
+fn takes_ownership(some_string: String) {
+    println!("value inside of take_ownership function: {}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("here is the copy of the i32 in the make_copy function: {}", some_integer);
 }
