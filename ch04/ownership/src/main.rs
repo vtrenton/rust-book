609bdc2d1e55f2ce0e1778a929df07f1c3b3a153
Trent V <trenton.vanderwert@gmail.com>
2023-12-19 19:19:52 -0600

    added some new vars and functions

--- ch04/ownership/src/main.rs
+++ ch04/ownership/src/main.rs
@@ -13,6 +13,11 @@ fn main() {
     
     // totally valid
     println!("after copy the value is still here local to main: {}", x);
+
+    let s1 = gives_ownership(); // gives_ownership sends its return value to s1
+    let s2 = String::from("Hello"); // s2 comes into scope
+    let s3 = takes_and_gives_back(s2); // 
+
 } // here x goes out of scope, s has already been moved into the take_ownership function
 
 fn takes_ownership(some_string: String) {
@@ -21,4 +26,13 @@ fn takes_ownership(some_string: String) {
 
 fn makes_copy(some_integer: i32) {
     println!("here is the copy of the i32 in the make_copy function: {}", some_integer);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string // return the String to the caller
+}
+
+fn takes_and_gives_back(a_string: String) -> {
+    
 }
