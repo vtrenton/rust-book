10412e97728df1b0aad42e4fef1c822c674ab9c7
Trent V <trenton.vanderwert@gmail.com>
2023-12-22 18:14:36 -0600

    created a reference example

--- ch04/refandborrow/src/main.rs
+++ ch04/refandborrow/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    
+    let s1 = String::from("hello");
+    // & is a reference to
+    // this keeps s1 from leaving scope
+    // s1 is 'borrowed' - the calling function maintains ownership
+    let s2 = calculate_length(&s1);  
+
+    println!("The len of '{}' is {}.", s1, s2);
+}
+
+// we declare the local variable as the type of 'reference' to the string
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
