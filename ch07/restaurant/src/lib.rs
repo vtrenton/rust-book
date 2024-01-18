7537cef5d8f93c566401fa168dc6fc33a16937bf
Trent V <trenton.vanderwert@suse.com>
2024-01-17 18:07:53 -0600

    Added some mods for chapter7
    Signed-off-by: Trent V <trenton.vanderwert@suse.com>

---
+++ ch07/restaurant/src/lib.rs
@@ -0,0 +1,77 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+mod back_of_house {
+    // with structs every field is private
+    // even if the struct is private
+    pub struct Breakfast {
+        // fields need to be declared as private
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        // Breakfast needs a public acocaited function
+        // within the scope of the Breakfast Struct
+        // because seasonal_fruit is private
+        // it cannot be constructed outside of the module scope
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+    
+    // Since the enum is public
+    // so are all the varients
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order()
+    }
+}
+
+use crate::front_of_house::hosting;
+
+pub fn eat_at_resturant() {
+    // Absolute Path
+    //crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative Pth
+    //front_of_house::hosting::add_to_waitlist();
+
+    // since we narrowed the scope with the use above
+    // we no longer need to specify front_of_house
+    // This is a lot like a symlink
+    hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change mind about bread
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // both varients can be accessed since the enum is public
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+fn deliver_order() {}
